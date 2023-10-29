5366f794d9f64c28b0e1a8df9d3e4397c47526cb
hoya <sv05159@naver.com>
2023-10-29 22:13:16 +0900

    Hamcrest

---
+++ src/test/HamcrestTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HamcrestTest {
+    @Test
+    public void testArray() throws Exception {
+        assertThat("Start Date 비교", "2023/10/20", is("2023/10/21"));
+//        java.lang.AssertionError: Start Date 비교
+//        Expected: is "2023/10/21"
+//        but: was "2023/10/20"
+//        Expected :2023/10/21
+//        Actual   :2023/10/20
+    }
+}
